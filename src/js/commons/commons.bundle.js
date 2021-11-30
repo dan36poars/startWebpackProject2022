2e3f090cf9d669813c3885c6e568dad6e2375797
Daniel Corrêa <eldestemido@gmail.com>
2021-11-30 13:35:07 -0300

    added fontawesome, gradient color true

--- src/js/commons/commons.bundle.js
+++ src/js/commons/commons.bundle.js
@@ -1,10 +1,18 @@
+// bootstrap
 import 'bootstrap'
 
+// commons
 import './commons.scss'
 
+// fontawesome-4.7
+import '../../../node_modules/fontawesome-4.7/css/font-awesome.min.css'
+import '../../../node_modules/fontawesome-4.7/fonts/fontawesome-webfont.svg'
+import '../../../node_modules/fontawesome-4.7/fonts/fontawesome-webfont.ttf'
+import '../../../node_modules/fontawesome-4.7/fonts/fontawesome-webfont.woff'
+import '../../../node_modules/fontawesome-4.7/fonts/fontawesome-webfont.woff2'
+import '../../../node_modules/fontawesome-4.7/fonts/FontAwesome.otf'
+
+// data files
 import notes from '../../data/data.csv'
 
 console.log(notes)
-
-
-
